fc2bc2c8dee0fd1e862c6ecd22ff90308c3371ee
Duck838 <162286400+Duck838@users.noreply.github.com>
2025-01-26 15:15:23 +0700

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7509665691:AAF-LgH09otfzNEPb77CgfxhX0SlVKLUjvM")
 PGUSER = os.getenv("PGUSER")
 PGPASSWORD = os.getenv("PGPASSWORD")
 DATABASE = str(os.getenv("DATABASE"))
